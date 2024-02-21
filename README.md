bcf914983a3e4d1d38d019a732477d1529bc14d2
EliotVonEcklie <eliotvonecklie@gmail.com>
2024-02-20 21:18:29 -0500

    Setup amethyst

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# grimplan
-# grimplan
+# Grimplan Game
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
-use bevy::prelude::*;
-use bevy_editor_pls::prelude::*;
-
-mod level;
-mod player;
-
-use crate::level::LevelPlugin;
-use crate::player::PlayerPlugin;
-
-pub fn main() {
-    App::new()
-        .add_plugins((DefaultPlugins, EditorPlugin::default()))
-        .add_plugins((LevelPlugin, PlayerPlugin))
-        .run();
+fn main() {
+    println!("Hello, world!");
 }
